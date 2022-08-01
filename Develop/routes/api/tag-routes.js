aca39fe660ad8757c3b6d20eef2f6f8e23bce33b
Rayncor <sowa332@gmail.com>
2022-08-01 09:02:35 -0700

    wrote product tag get routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,16 +6,38 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+//our tag routes will include the product they are associated with. no need to tie producttag model.
+router.get('/', async (req, res) => {
   // find all tags
+  try {
+    //find all products with a tag. no need for 404 because we are not searching for particular ids yet. 
+    const findTag = await Tag.findAll({ include: Product });
+    res.status(200).json(findTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+//this can be performed in an identical manner to our get by id in product and category routes
+router.get('/:id', async (req, res) => {
+  try {
+    const findTagById = await Tag.findByPk(req.params.id, { include: Product });
+
+    if (!findTagById) {
+      res.status(404).json({ message: "We're sorry- no tag matching that id has been found in our database."});
+      return;
+    }
+    res.status(200).json(findTagById);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
+
+//TODO: not entirely sure how to write this one. Need to read up on post routes involving async and create req bodies
 router.post('/', (req, res) => {
   // create a new tag
 });
